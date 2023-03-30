fadf9c84017079158f655abde0895c027d02f17d
Angel Moisés Escoto Irías <angel.escoto2@ujcv.edu.hn>
2023-03-29 18:47:33 -0600

    Creando entidad Rol

--- TallerMecanico/Models/ViewModels/RolVm.cs
+++ TallerMecanico/Models/ViewModels/RolVm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Examen.Models.ViewModels;
+using System;
 
 namespace TallerMecanico.Models.ViewModels
 {
@@ -7,5 +8,78 @@ namespace TallerMecanico.Models.ViewModels
         public Guid Id { get; set; }
         public string Descripcion { get; set; }
         public string Descripcion2 { get; set; }
+        public AppResultVm Validar()
+        {
+            AppResultVm app = new AppResultVm();
+
+            if (string.IsNullOrEmpty(this.Descripcion))
+            {
+                app.Mensaje += "La descripcion no puede esta vacío \n";
+
+            }
+            if (string.IsNullOrEmpty(this.Descripcion2))
+            {
+                app.Mensaje += "La descripcion no puede esta vacío \n";
+
+            }
+            if (string.IsNullOrEmpty(app.Mensaje))
+            {
+                app.IsValid = true;
+                app.Mensaje = "Registro finalizado con éxito.";
+            }
+            else
+            {
+                app.IsValid = false;
+            }
+            return app;
+        }
+        public AppResultVm ValidarEnUpdate()
+        {
+            AppResultVm app = new AppResultVm();
+
+            if (this.Id == Guid.Empty)
+            {
+                app.Mensaje += "El ID no debe estar vacio \n";
+            }
+            if (string.IsNullOrEmpty(this.Descripcion))
+            {
+                app.Mensaje += "La descripcion no puede esta vacío \n";
+
+            }
+            if (string.IsNullOrEmpty(this.Descripcion2))
+            {
+                app.Mensaje += "La descripcion no puede esta vacío \n";
+
+            }
+            if (string.IsNullOrEmpty(app.Mensaje))
+            {
+                app.IsValid = true;
+                app.Mensaje = "Registro finalizado con éxito.";
+            }
+            else
+            {
+                app.IsValid = false;
+            }
+            return app;
+        }
+        public AppResultVm ValidarEnDelete()
+        {
+            AppResultVm app = new AppResultVm();
+
+            if (this.Id == Guid.Empty)
+            {
+                app.Mensaje += "El ID no debe estar vacio \n";
+            }
+            if (string.IsNullOrEmpty(app.Mensaje))
+            {
+                app.IsValid = true;
+                app.Mensaje = "Registro eliminado con éxito.";
+            }
+            else
+            {
+                app.IsValid = false;
+            }
+            return app;
+        }
     }
 }
